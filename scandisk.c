e66f1d482274c59f58b132fd5f9ccab7b4962af4
jsneer23 <jsneeringer@colgate.edu>
2014-12-12 22:33:52 -0500

    Guess who's back? Back again. Shady's back. Tell a friend.

--- scandisk.c
+++ scandisk.c
@@ -15,6 +15,47 @@
 #include "fat.h"
 #include "dos.h"
 
+void get_name(char *fullname, struct direntry *dirent) 
+{
+    char name[9];
+    char extension[4];
+    int i;
+
+    name[8] = ' ';
+    extension[3] = ' ';
+    memcpy(name, &(dirent->deName[0]), 8);
+    memcpy(extension, dirent->deExtension, 3);
+
+    /* names are space padded - remove the padding */
+    for (i = 8; i > 0; i--) 
+    {
+    if (name[i] == ' ') 
+        name[i] = '\0';
+    else 
+        break;
+    }
+
+    /* extensions aren't normally space padded - but remove the
+       padding anyway if it's there */
+    for (i = 3; i > 0; i--) 
+    {
+    if (extension[i] == ' ') 
+        extension[i] = '\0';
+    else 
+        break;
+    }
+    fullname[0]='\0';
+    strcat(fullname, name);
+
+    /* append the extension if it's not a directory */
+    if ((dirent->deAttributes & ATTR_DIRECTORY) == 0) 
+    {
+    strcat(fullname, ".");
+    strcat(fullname, extension);
+    }
+}
+
+
 uint16_t getfollowclust(struct direntry *dirent, int indent) {     
     uint16_t followclust = 0;      
        
@@ -49,6 +90,36 @@ uint16_t getfollowclust(struct direntry *dirent, int indent) {
     return followclust;        
 }     
 
+
+void trace(struct direntry *dirent, uint8_t *image_buf, struct bpb33* bpb, uint8_t *BFA){
+	uint16_t cluster = getushort(dirent->deStartCluster);
+    uint16_t oldCluster = 0;
+	int size = 0; //There is no cluster 0?
+	while (!is_end_of_file(cluster)){
+        if(cluster == (CLUST_BAD & FAT12_MASK)) {
+            if(oldCluster != 0) {
+                set_fat_entry(oldCluster,CLUST_EOFS & FAT12_MASK,image_buf,bpb);
+            }
+            break;
+        }
+        size += 512; 
+
+        oldCluster = cluster;
+        cluster = get_fat_entry(cluster, image_buf, bpb);
+		printf("here\n");
+	}
+           
+    int difference = size - (int)getulong(dirent->deFileSize);
+
+    //Solution to case where file in directory is too large, reset to FAT table size
+    if(difference < 0) {
+		char name[14];
+		get_name(name,dirent);
+        printf("%s is too small\n", name);
+	}
+     
+}
+
 void follow_dir(uint16_t cluster, int indent, uint8_t *image_buf, struct bpb33* bpb, uint8_t *BFA) {
     
 	while (is_valid_cluster(cluster, bpb)) {
@@ -58,7 +129,7 @@ void follow_dir(uint16_t cluster, int indent, uint8_t *image_buf, struct bpb33* 
 		for ( ; i < numDirEntries; i++){
             uint16_t followclust = getfollowclust(dirent, indent);
 			if(getushort(dirent->deStartCluster) != 0){
-				//trace(dirent, image_buf, bpb, BFA);                
+				trace(dirent, image_buf, bpb, BFA);                
 			}
             if (followclust)
                 follow_dir(followclust, indent+1, image_buf, bpb, BFA);
@@ -168,47 +239,6 @@ void create_dirent(struct direntry *dirent, char *filename,
     dirent++;
     }
 }
-
-void get_name(char *fullname, struct direntry *dirent) 
-{
-    char name[9];
-    char extension[4];
-    int i;
-
-    name[8] = ' ';
-    extension[3] = ' ';
-    memcpy(name, &(dirent->deName[0]), 8);
-    memcpy(extension, dirent->deExtension, 3);
-
-    /* names are space padded - remove the padding */
-    for (i = 8; i > 0; i--) 
-    {
-    if (name[i] == ' ') 
-        name[i] = '\0';
-    else 
-        break;
-    }
-
-    /* extensions aren't normally space padded - but remove the
-       padding anyway if it's there */
-    for (i = 3; i > 0; i--) 
-    {
-    if (extension[i] == ' ') 
-        extension[i] = '\0';
-    else 
-        break;
-    }
-    fullname[0]='\0';
-    strcat(fullname, name);
-
-    /* append the extension if it's not a directory */
-    if ((dirent->deAttributes & ATTR_DIRECTORY) == 0) 
-    {
-    strcat(fullname, ".");
-    strcat(fullname, extension);
-    }
-}
-
 
 #define FIND_FILE 0
 #define FIND_DIR 1
@@ -361,7 +391,6 @@ int main(int argc, char** argv) {
 		int16_t followclust = getfollowclust(dirent, 0);
 		if (is_valid_cluster(followclust, bpb)){
 			follow_dir(followclust, 1, image_buf, bpb, BFA);
-			printf("followclust");
 		}
 		dirent++;
 	}
@@ -372,6 +401,7 @@ int main(int argc, char** argv) {
     //Not shit
     unmmap_file(image_buf, &fd);
     free(BFA);
+	free(bpb);
 
 
     return 0;
